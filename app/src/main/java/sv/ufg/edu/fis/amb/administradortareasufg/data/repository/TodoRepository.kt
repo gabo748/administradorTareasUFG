4e8b9fbe7fca3171640e21ebae2dbe9990b25fd7
Gabriel Campos <gaeducampos@gmail.com>
2024-04-12 15:56:26 -0600

    Incorporate Repository
    - Repository is in charge of handle the logic that will store, update or delete the data.

---
+++ app/src/main/java/sv/ufg/edu/fis/amb/administradortareasufg/data/repository/TodoRepository.kt
@@ -0,0 +1,45 @@
+package sv.ufg.edu.fis.amb.administradortareasufg.data.repository
+
+import android.content.Context
+import com.google.gson.Gson
+import sv.ufg.edu.fis.amb.administradortareasufg.data.model.Todo
+import sv.ufg.edu.fis.amb.administradortareasufg.util.MySharedPreferences
+
+class TodoRepository {
+    private val jsonParser = Gson()
+
+    fun updateTodos(
+        todos: MutableList<Todo>,
+        todoToUpdate: Todo,
+        context: Context
+    ) {
+        // Find the index of the todo to update in the list
+        val index = todos.indexOfFirst { it.id == todoToUpdate.id }
+
+        // Check if the todo exists in the list
+        if (index != -1) {
+            // Update the todo with the new values
+            todos[index] = todoToUpdate
+            // Save the updated todos list to SharedPreferences or perform any other necessary actions
+            MySharedPreferences.saveJsonData(context, jsonParser.toJson(todos))
+        }
+    }
+
+    fun deleteTodo(
+        todos: MutableList<Todo>,
+        todoToDelete: Todo,
+        context: Context
+    ) {
+        todos.removeIf { it.id == todoToDelete.id }
+        MySharedPreferences.saveJsonData(context, jsonParser.toJson(todos))
+    }
+
+    fun insertTodo(
+        todos: MutableList<Todo>,
+        todoToInsert: Todo,
+        context: Context
+    ) {
+        todos.add(todoToInsert)
+        MySharedPreferences.saveJsonData(context, jsonParser.toJson(todos))
+    }
+}
