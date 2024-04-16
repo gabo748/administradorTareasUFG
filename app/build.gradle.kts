1e37e3d4b932c01486fabeb0610b7ac9381f0c8c
Marvin Ramos <mr487223@gmail.com>
2024-04-16 17:09:11 -0600

    add cardview dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
 dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.cardview:cardview:1.0.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
