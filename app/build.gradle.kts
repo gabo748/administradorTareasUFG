c942c3e1f89429358c66b605e2e938cdd3d81a1f
Anthony Miguel Hernandez Guido <guidohdezz29@gmail.com>
2024-04-17 13:16:59 -0600

    Design UI for the details task fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,8 @@ android {
 }
 
 dependencies {
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
