35fed97a96d802ca9b3f04e61c35dbda1b8f5cd6
Anthony Miguel Hernandez Guido <guidohdezz29@gmail.com>
2024-04-26 12:21:07 -0600

    Added new checkbox and enabled Scrollview

--- app/src/main/java/sv/ufg/edu/fis/amb/administradortareasufg/ui/view/task_details_fragment.kt
+++ app/src/main/java/sv/ufg/edu/fis/amb/administradortareasufg/ui/view/task_details_fragment.kt
@@ -39,8 +39,16 @@ class task_details_fragment : Fragment() {
         val checkBox = view.findViewById<MaterialCheckBox>(R.id.status1)
         val checkBox2 = view.findViewById<MaterialCheckBox>(R.id.status2)
         val checkBox3 = view.findViewById<MaterialCheckBox>(R.id.status3)
+        val checkBox4 = view.findViewById<MaterialCheckBox>(R.id.ck_today)
+        val checkBox5 = view.findViewById<MaterialCheckBox>(R.id.ck_thisWeek)
+        val checkBox6 = view.findViewById<MaterialCheckBox>(R.id.ck_twoWeeks)
+        val checkBox7 = view.findViewById<MaterialCheckBox>(R.id.priority1)
+        val checkBox8 = view.findViewById<MaterialCheckBox>(R.id.priority2)
+        val checkBox9 = view.findViewById<MaterialCheckBox>(R.id.priority3)
 
         val checkBoxList = listOf(checkBox, checkBox2, checkBox3)
+        val checkBoxList2 = listOf(checkBox4, checkBox5, checkBox6)
+        val checkBoxList3 = listOf(checkBox7, checkBox8, checkBox9)
 
         checkBox.setOnCheckedChangeListener { buttonView, isChecked ->
             if (isChecked) {
@@ -75,6 +83,71 @@ class task_details_fragment : Fragment() {
             }
         }
 
+        checkBox4.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBoxList2.forEach { cb ->
+                    if (cb != buttonView && cb.isChecked) {
+                        cb.isChecked = false
+                    }
+                }
+                Toast.makeText(context, "Su tarea se entrega HOY!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        checkBox5.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBoxList2.forEach { cb ->
+                    if (cb != buttonView && cb.isChecked) {
+                        cb.isChecked = false
+                    }
+                }
+                Toast.makeText(context, "Su tarea se entra ESTA SEMANA!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        checkBox6.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBoxList2.forEach { cb ->
+                    if (cb != buttonView && cb.isChecked) {
+                        cb.isChecked = false
+                    }
+                }
+                Toast.makeText(context, "Su tarea se entrega en DOS SEMANAS!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        checkBox7.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBoxList3.forEach { cb ->
+                    if (cb != buttonView && cb.isChecked) {
+                        cb.isChecked = false
+                    }
+                }
+                Toast.makeText(context, "El nivel de prioridad de su tarea es NORMAL!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        checkBox8.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBoxList3.forEach { cb ->
+                    if (cb != buttonView && cb.isChecked) {
+                        cb.isChecked = false
+                    }
+                }
+                Toast.makeText(context, "El nivel de prioridad de su tarea es MEDIA!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        checkBox9.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                checkBoxList3.forEach { cb ->
+                    if (cb != buttonView && cb.isChecked) {
+                        cb.isChecked = false
+                    }
+                }
+                Toast.makeText(context, "El nivel de prioridad de su tarea es ALTA!", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         return view
     }
