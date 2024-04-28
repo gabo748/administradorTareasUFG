a5c7ae85ac61bf85fc9b7f88d479ea932807b667
Gabriel Campos <gaeducampos@gmail.com>
2024-04-27 19:18:28 -0600

    removed unnecesary data from fragments

--- app/src/main/java/sv/ufg/edu/fis/amb/administradortareasufg/ui/view/task_details_fragment.kt
+++ app/src/main/java/sv/ufg/edu/fis/amb/administradortareasufg/ui/view/task_details_fragment.kt
@@ -8,26 +8,9 @@ import android.view.ViewGroup
 import android.widget.Toast
 import com.google.android.material.checkbox.MaterialCheckBox
 import sv.ufg.edu.fis.amb.administradortareasufg.R
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [task_details_fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class task_details_fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
