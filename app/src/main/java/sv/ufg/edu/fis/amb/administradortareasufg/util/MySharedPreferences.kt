69ba4a4c9ec61c676125b9374be7a2d717f92d43
Gabriel Campos <gaeducampos@gmail.com>
2024-04-27 19:20:50 -0600

    updated naming in keys stores in device

--- app/src/main/java/sv/ufg/edu/fis/amb/administradortareasufg/util/MySharedPreferences.kt
+++ app/src/main/java/sv/ufg/edu/fis/amb/administradortareasufg/util/MySharedPreferences.kt
@@ -3,8 +3,8 @@ package sv.ufg.edu.fis.amb.administradortareasufg.util
 import android.content.Context
 
 object MySharedPreferences {
-    private const val SHARED_PREF_NAME = "todos_mock"
-    private const val KEY_JSON_DATA = "todos_json_data"
+    private const val SHARED_PREF_NAME = "todos_mock_list_data"
+    private const val KEY_JSON_DATA = "todos_json_list_data"
 
     fun saveJsonData(context: Context, jsonData: String) {
         val sharedPreferences = context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE)
