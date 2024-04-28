978b9202d0201b3eca7d33aaa1f20aead6b42442
Gabriel Campos <gaeducampos@gmail.com>
2024-04-27 19:17:57 -0600

    pass todo as paramenter to fragment

--- app/src/main/java/sv/ufg/edu/fis/amb/administradortareasufg/ui/view/MainActivity.kt
+++ app/src/main/java/sv/ufg/edu/fis/amb/administradortareasufg/ui/view/MainActivity.kt
@@ -14,8 +14,10 @@ import sv.ufg.edu.fis.amb.administradortareasufg.R
 import sv.ufg.edu.fis.amb.administradortareasufg.data.model.DateRange
 import sv.ufg.edu.fis.amb.administradortareasufg.data.model.Todo
 import sv.ufg.edu.fis.amb.administradortareasufg.data.model.TodoPriority
+import sv.ufg.edu.fis.amb.administradortareasufg.data.model.TodoStatus
 import sv.ufg.edu.fis.amb.administradortareasufg.ui.viewModel.TodoViewModel
 import sv.ufg.edu.fis.amb.administradortareasufg.util.MySharedPreferences
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,29 +26,23 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+                val todo = Todo(
+            topic = "Complete assignment",
+            description = "Finish the project by Friday",
+            status = TodoStatus.started,
+            priority = TodoPriority.hard,
+            doDate = Date(), // Sample date string
+            dateRange = DateRange.thisWeek // Sample date range
+        )
         //CODIGO QUE CORRE EL FRAGMENTO AL CARGAR
-        val taskDetailsFragment = task_details_fragment()
+        // Se le pasa por constructor al fragmento, el todo previamente declarado
+        // asi se muestra su info en la view
+        val taskDetailsFragment = task_details_fragment(todo = todo)
         supportFragmentManager.beginTransaction()
             .add(R.id.fragment_container, taskDetailsFragment)
             .commit()
         //----------------------------------------
 
 
-        viewModel = ViewModelProvider(this)[TodoViewModel::class.java]
-
-        val todosString = MySharedPreferences.getJsonData(this)
-        if (todosString != null) {
-            val typeToken = object : TypeToken<MutableList<Todo>>() {}.type
-            val todos: MutableList<Todo> = Gson().fromJson(todosString, typeToken)
-            viewModel.setTodos(todos)
-
-            Log.d("Todos", "The value of x is: ${viewModel.todos.value}")
-
-        }
-
-        // observer to update the UI
-        viewModel.todos.observe(this, Observer { todos ->
-
-        })
     }
 }
